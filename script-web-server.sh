b60949f2c1209e252b3252aa1ce66bffab81127e
Tiago Valadares <tiago.rv.elt@gmail.com>
2025-04-11 19:34:19 -0300

    Fix: Unzip do arquivo baixado e adição de comentários

--- script-web-server.sh
+++ script-web-server.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
 echo "Script de Provisionamento de um Servidor Web (Apache)"
+
 cd /
-apt update
-apt upgrade -y
+
+echo "Atualizando o servidor"
+apt-get update
+apt-get upgrade -y
 apt install apache2 -y
 apt install unzip -y
 
 cd /tmp
+
+echo "Baixando os arquivos da aplicação"
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
-cd linux-site-dio
-cp -R * /var/www/html
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
